13c9ae26d33b6a227c0fbdc07495d849518e98bf
iamirinad <irka_dimcheva@abv.bg>
2023-02-09 15:51:06 +0200

    addUser thunk implemented

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchUsers } from '../+store';
+import { fetchUsers, addUser } from '../+store';
+import Button from './Button';
 import Skeleton from './Skeleton';
 
 function UsersList() {
@@ -13,6 +14,10 @@ function UsersList() {
     dispatch(fetchUsers());
   }, [dispatch]);
 
+  const handleUserAdd = () => {
+    dispatch(addUser());
+  };
+
   if (isLoading) {
     return <Skeleton times={6} className="h-10 w-full" />;
   }
@@ -21,9 +26,25 @@ function UsersList() {
     return <div>Error fetching data...</div>;
   }
 
+  const renderedUsers = data.map((user) => {
+    return (
+      <div key={user.id} className="mb-2 border rounded">
+        <div className="flex p-2 justify-between items-center cursor-pointer">
+          {user.name}
+        </div>
+      </div>
+    );
+  });
+
   return (
     <div>
-      {data.length}
+      <div className="flex flex-row justify-between m-3">
+        <h1 className="m-2 text-xl">Users</h1>
+        <Button onClick={handleUserAdd}>
+          + Add User
+        </Button>
+      </div>
+      {renderedUsers}
     </div>
   );
 }
