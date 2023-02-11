d79a3e30f079084164be15791499bd9e3f214f86
iamirinad <irka_dimcheva@abv.bg>
2023-02-11 19:36:27 +0200

    implemented delete user

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -4,6 +4,7 @@ import { fetchUsers, addUser } from '../+store';
 import { useThunk } from '../hooks/use-thunk';
 import Button from './Button';
 import Skeleton from './Skeleton';
+import UsersListItem from './UsersListItem';
 
 function UsersList() {
   const [doFetchUsers, isLoadingUsers, loadingUsersError] = useThunk(fetchUsers);
@@ -27,13 +28,7 @@ function UsersList() {
     content = <div>Error fetching data...</div>;
   } else {
     content = data.map((user) => {
-      return (
-        <div key={user.id} className="mb-2 border rounded">
-          <div className="flex p-2 justify-between items-center cursor-pointer">
-            {user.name}
-          </div>
-        </div>
-      );
+      return <UsersListItem key={user.id} user={user} />;
     });
   }
 
